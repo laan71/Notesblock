71f4a5f7467318023d9ab86c6d2dc491cfb96847
sune <sune>
2020-04-23 17:02:12 +0200

    made som changes

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,15 +4,17 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 600, 600));
+        primaryStage.setTitle("NotesBlok");
+        VBox vBox = new VBox();
+        Scene scene = new Scene(vBox, 600, 600);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
