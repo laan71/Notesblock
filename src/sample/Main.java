2213b4db5e6f45d0a08d864f417b9ef6e6acc1ae
sune <sune>
2020-04-23 17:14:32 +0200

    made som changes

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,20 +1,24 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
+import javafx.scene.web.HTMLEditor;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        primaryStage.setTitle("NotesBlok");
-        VBox vBox = new VBox();
-        Scene scene = new Scene(vBox, 600, 600);
+    public void start(Stage primaryStage) {
+
+        HTMLEditor htmlEditor = new HTMLEditor();
+
+        VBox vBox = new VBox(htmlEditor);
+        Scene scene = new Scene(vBox);
+
         primaryStage.setScene(scene);
+        primaryStage.setTitle("NotesBlok");
+
         primaryStage.show();
     }
 
