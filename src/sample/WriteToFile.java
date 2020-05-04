91a7e280aaf20187e888614843d3644eaf8c4a76
laan71 <lail5198@edu.easj.dk>
2020-05-04 22:09:07 +0200

    I have added runconfig main to gitignore

--- src/sample/WriteToFile.java
+++ src/sample/WriteToFile.java
@@ -7,7 +7,7 @@ public class WriteToFile {
     public static void main(String[] args) {
         try {
             FileWriter myWriter = new FileWriter("filename.txt");
-            myWriter.write("Files in Java can be tricky!");
+            myWriter.write("java");
             myWriter.close();
             System.out.println("You successfully wrote to the file.");
         } catch (IOException e) {
