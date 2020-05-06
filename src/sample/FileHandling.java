b189ebe008a83b6008150f94d3cb19c1bfe35633
laan71 <lail5198@edu.easj.dk>
2020-05-06 12:30:21 +0200

    Filehandler is finished with CRUD (create, read, update and delete)

--- src/sample/FileHandling.java
+++ src/sample/FileHandling.java
@@ -1,15 +1,17 @@
 package sample;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 // CRUD for notesblock
 public class FileHandling {
 
-    public static void CreateFile(String fileName){ // FileHandling f = new FileHandling(); f.createFile("Hest.txt"); f.createFile("Ost.html"); 
+    public static void createFile(String fileName){ // FileHandling f = new FileHandling(); f.createFile("Hest.txt"); f.createFile("Ost.html");
         try {
-            File myObj = new File("filename.txt");
+            File myObj = new File(fileName);
             if (myObj.createNewFile()) {
                 System.out.println("File created: " + myObj.getName());
             } else {
@@ -21,40 +23,52 @@ public class FileHandling {
         }
     }
 
-    public static void ReadFile(String[] args) {
+    public static void readFile(String fileName) {
         try {
-        // @TODO
-            FileWriter readFile = new FileWriter("filename.txt");
+            File myObj = new File(fileName);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+     /*   try {
+
+            FileWriter readFile = new FileWriter(fileName);
 
         } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
-        }
+        } */
     }
 
-    public static void UpdateFile(String fileName, String fileContent) { // Både filnavn og indhold skal være argument
+    public static void updateFile(String fileName, String fileContent) { // Både filnavn og indhold skal være argument
         try {
-            FileWriter myWriter = new FileWriter("filename.txt");
-            myWriter.write("java");
+            FileWriter myWriter = new FileWriter(fileName);
+            myWriter.write(fileContent);
             myWriter.close();
-            System.out.println("You successfully wrote to the file.");
+            System.out.println("You successfully updated the file.");
         } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
     }
 
-    public static void DeleteFile(String[] args) {
+    public static void deleteFile(String fileName) {
         try {
             // Tanker på delete funktion endnu ikke færdig
-            FileWriter deleteFile = new FileWriter("filename.txt");
+         //   FileWriter deleteFile = new FileWriter(fileName);
 
-            File myObj = new File("filename.txt");
+            File myObj = new File(fileName);
             if (myObj.delete()) {
                 System.out.println("You successfully deleted the file: " + myObj.getName());
             }
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
+        } catch(Exception e) {
+            System.out.println("An error occurred");
             e.printStackTrace();
         }
     }
